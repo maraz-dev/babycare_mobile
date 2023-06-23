ddaafcd2c4e9683ae82811eb8aa20582ac696806
Ayodele Fagbami <afagbami@gmail.com>
2023-06-23 14:03:26 +0100

    added articles URL launcher for articles

--- lib/features/baby_care/presentation/pages/ui/home_screen/knowledge_center.dart
+++ lib/features/baby_care/presentation/pages/ui/home_screen/knowledge_center.dart
@@ -43,14 +43,67 @@ class KnowledgeCenter extends StatelessWidget {
                 ),
               ],
             ),
-            KnowledgeCenterWidget(
-              press: () {
-                // return Navigator.push(context,
-                //     MaterialPageRoute(builder: (context) => const Settings()));
-              },
+            const SizedBox(height: 20),
+            const KnowledgeCenterWidget(
+              image: "https://img.youtube.com/vi/15lI2QT61Ro/sddefault.jpg",
+              title: "The First Trimester--Weeks 1-13!",
+              description:
+                  "Week by Week Pregnancy Guide and Pregnancy Vlog with an OB/GYN & Pediatrician",
+              link:
+                  "https://www.youtube.com/watch?v=15lI2QT61Ro&list=PLYMcLHAKovpGoRS6RkitJTa0iAP51hCRp",
             ),
-            const KnowledgeCenterWidget(),
-            const KnowledgeCenterWidget(),
+            const SizedBox(height: 20),
+            const KnowledgeCenterWidget(
+              image: "https://img.youtube.com/vi/Rnd68avWFK0/sddefault.jpg",
+              title: "The Second Trimester--Weeks 14- 28!",
+              description:
+                  "Choosing a Doctor for Your Baby: A Pregnancy MUST DO!",
+              link:
+                  "https://www.youtube.com/watch?v=Rnd68avWFK0&list=PLYMcLHAKovpF4Y7-gVqF0zrcucay5SR1z",
+            ),
+            const SizedBox(height: 20),
+            const KnowledgeCenterWidget(
+              image: "https://img.youtube.com/vi/pSQlV5XAZqQ/sddefault.jpg",
+              title: "The Third Trimester--Weeks 28-40!",
+              description:
+                  "Am I In Labor? -- What are Braxton Hicks, what does labor look like, and when to call your OB?",
+              link:
+                  "https://www.youtube.com/watch?v=pSQlV5XAZqQ&list=PLYMcLHAKovpEQ0XOO87920A2hSxpF95LC",
+            ),
+            const SizedBox(height: 20),
+            const KnowledgeCenterWidget(
+              image: "https://img.youtube.com/vi/81dQFYIJP60/sddefault.jpg",
+              title: "Vlogs",
+              description:
+                  "What I Wish I Knew About the First Trimester | OB-GYN Pregnancy VLOG",
+              link:
+                  "https://www.youtube.com/watch?v=81dQFYIJP60&list=PLYMcLHAKovpHTaFk2zZqx9ubpF4_uC3_O",
+            ),
+            const SizedBox(height: 20),
+            const KnowledgeCenterWidget(
+              image: "https://img.youtube.com/vi/fqRYHXtT9lk/sddefault.jpg",
+              title: "Antenatal Exercises… Keep Fit!",
+              description:
+                  "Antenatal Exercise / exercises can do during pregnancy",
+              link: "https://www.youtube.com/watch?v=fqRYHXtT9lk",
+            ),
+            const SizedBox(height: 20),
+            const KnowledgeCenterWidget(
+              image: "https://img.youtube.com/vi/scGB5LEnxI4/sddefault.jpg",
+              title: "Sleeping Positions",
+              description:
+                  "Wrong Sleeping Positions For Pregnant Women Harm the Fetus | Best Sleeping Position during Pregnancy",
+              link: "https://www.youtube.com/watch?v=scGB5LEnxI4",
+            ),
+            const SizedBox(height: 20),
+            const KnowledgeCenterWidget(
+              image: "https://img.youtube.com/vi/WH9ZJu4wRUE/sddefault.jpg",
+              title: "Fetal Development",
+              description:
+                  "9 Months In The Womb: A Remarkable Look At Fetal Development Through Ultrasound",
+              link: "https://www.youtube.com/watch?v=WH9ZJu4wRUE",
+            ),
+            const SizedBox(height: 20),
           ],
         ),
       ),
--- lib/features/baby_care/presentation/pages/ui/home_screen/widgets/knowledgeCenterWidget.dart
+++ lib/features/baby_care/presentation/pages/ui/home_screen/widgets/knowledgeCenterWidget.dart
@@ -1,30 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 import '../../../../../../../common/commons.dart';
+
 class KnowledgeCenterWidget extends StatelessWidget {
-  final press;
-  const KnowledgeCenterWidget({Key? key, this.press}) : super(key: key);
+  const KnowledgeCenterWidget({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.description,
+    required this.link,
+  }) : super(key: key);
+  final String image;
+  final String title;
+  final String description;
+  final String link;
 
   @override
   Widget build(BuildContext context) {
+    final websiteUri = Uri.parse(link);
     return InkWell(
-      onTap: press,
-      child: Container(
-        height: 200,
-        padding: EdgeInsets.all(25),
-        //margin: EdgeInsets.symmetric(vertical: 0),
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          image: DecorationImage(image: AssetImage('assets/dog.png'))
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('12 Jul 2020', style: normalText.copyWith(color: Colors.white, fontSize: 10),),
-            Spacer(),
-            Text('Dogs are carrying the virus', style: headerText.copyWith(color: Colors.white, fontSize: 22),),
-            Text('Some Description', style: normalText.copyWith(color: Colors.white, fontSize: 14),),
-          ],
-        ),
+      onTap: () async {
+        if (await canLaunchUrl(websiteUri)) {
+          await launchUrl(websiteUri);
+        }
+      },
+      child: Stack(
+        children: [
+          Container(
+            height: 168,
+            //margin: EdgeInsets.symmetric(vertical: 0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              image: DecorationImage(
+                image: NetworkImage(
+                  image,
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.black.withOpacity(0.5),
+            ),
+            height: 168,
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '12 Jul 2020',
+                  style: normalText.copyWith(color: Colors.white, fontSize: 10),
+                ),
+                Spacer(),
+                Text(
+                  title,
+                  style: headerText.copyWith(color: Colors.white, fontSize: 22),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  description,
+                  style: normalText.copyWith(color: Colors.white, fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
